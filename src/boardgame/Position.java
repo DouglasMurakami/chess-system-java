41495036db7759292fa92e5b85c1dad619eede6b
Douglas Murakami <murakami2009@hotmail.com>
2022-09-23 02:29:20 -0300

    Chess layer and printing the board

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -4,17 +4,17 @@ public class Position {
 
 	private int row;
 	private int column;
-	
+
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
 
-	public int getrow() {
+	public int getRow() {
 		return row;
 	}
 
-	public void setrow(int row) {
+	public void setRow(int row) {
 		this.row = row;
 	}
 
@@ -30,14 +30,5 @@ public class Position {
 	public String toString() {
 		return row + ", " + column;
 	}
-	
-	
-	
 
 }
-
-
-
-
-
-
